69945048daed4afd369b76016b366aaae64a5565
Johnny McQuade <jfmcquade@hotmail.com>
2024-07-10 12:23:28 +0100

    chore: init

---
+++ config.ts
@@ -0,0 +1,16 @@
+import { generateDeploymentConfig } from "scripts";
+const config = generateDeploymentConfig("virtual_maths_camp")
+
+config.google_drive = {
+  sheets_folder_ids: ["1Iso9GuH6XTDCUFyKToFnjmFNm1Jjt3BB"],
+  assets_folder_ids: ["1PFerozfE6i97cMQZNZeo8gA2kHgYlXqI"],
+}
+
+config.git.content_repo = "https://github.com/IDEMSInternational/vmc-app-content"
+config.git.content_tag_latest = "0.0.0"
+
+// Override any app constants here
+config.app_config.APP_HEADER_DEFAULTS.title = "Virtual Maths Camp"
+config.app_config.APP_SIDEMENU_DEFAULTS.title = "Virtual Maths Camp"
+  
+export default config;
