ee63d770449051e5adfc85d536b1db444d8fe80a
Johnny McQuade <jfmcquade@hotmail.com>
2024-07-10 12:39:36 +0100

    chore: fix git config

--- config.ts
+++ config.ts
@@ -6,11 +6,13 @@ config.google_drive = {
   assets_folder_ids: ["1PFerozfE6i97cMQZNZeo8gA2kHgYlXqI"],
 }
 
-config.git.content_repo = "https://github.com/IDEMSInternational/vmc-app-content"
-config.git.content_tag_latest = "0.0.0"
+config.git = {
+  content_repo: "https://github.com/IDEMSInternational/vmc-app-content",
+  content_tag_latest: "0.0.0"
+}
 
 // Override any app constants here
 config.app_config.APP_HEADER_DEFAULTS.title = "Virtual Maths Camp"
 config.app_config.APP_SIDEMENU_DEFAULTS.title = "Virtual Maths Camp"
-  
+
 export default config;
